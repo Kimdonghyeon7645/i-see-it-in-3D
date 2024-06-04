4a6a4667931c3c4472282fbec8e4f3732b3456cc
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-06-04 18:38:49 +0900

    code : 큐브 생성 함수화

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
 import * as PIXI3D from "pixi3d";
 
+function initCube(position) {
+  let cube = PIXI3D.Mesh3D.createCube();
+  cube.position.set(...position); // Set the position (x, y, z)
+  cube.scale.set(2, 2, 0.05);    // Set the scale (width, height, depth)
+  console.log(cube.rotation);
+  const material = new PIXI3D.StandardMaterial();
+  material.baseColor = new PIXI3D.Color(0.95, 0.95, 0.9); // RGB values between 0 and 1
+  cube.material = material;
+
+  return cube;
+}
+
 const App = () => {
   const pixiContainer = useRef(null);
   useEffect(() => {
@@ -13,18 +25,15 @@ const App = () => {
       antialias: true,
     });
     pixiContainer.current.appendChild(app.view);
-
-    let mesh = app.stage.addChild(PIXI3D.Mesh3D.createCube());
+    app.stage.addChild(initCube([0, 0, 0]));
+    app.stage.addChild(initCube([0, 0, 1]));
 
     let light = new PIXI3D.Light();
-    light.position.set(-1, 0, 2);
+    light.position.set(-3, 3, 2);
+    light.intensity = 100;
     PIXI3D.LightingEnvironment.main.lights.push(light);
 
-    let rotation = 0;
-    app.ticker.add(() => {
-      rotation += 0.5
-      mesh.rotationQuaternion.setEulerAngles(0, rotation, 0);
-    });
+    new PIXI3D.CameraOrbitControl(app.view);
   }, []);
 
   return <div ref={pixiContainer} />;
