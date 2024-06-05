a243083b82f577c8d5a4023789c04fb2331cf6dd
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-06-05 10:57:39 +0900

    code : 색상이랑 회전 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
 import * as PIXI3D from "pixi3d";
 
-function initCube(position) {
+function initCube(position, rotation) {
   let cube = PIXI3D.Mesh3D.createCube();
   cube.position.set(...position); // Set the position (x, y, z)
-  cube.scale.set(2, 2, 0.05);    // Set the scale (width, height, depth)
-  console.log(cube.rotation);
+  cube.scale.set(2, 2, 0.05); // Set the scale (width, height, depth)
   const material = new PIXI3D.StandardMaterial();
-  material.baseColor = new PIXI3D.Color(0.95, 0.95, 0.9); // RGB values between 0 and 1
+  material.baseColor = new PIXI3D.Color(0.85, 0.81, 0.73); // RGB values between 0 and 1
+  material.metallic = 0.2;
+  material.roughness = 0.7;
   cube.material = material;
+  console.log(cube.materials)
+  cube.rotationQuaternion.setEulerAngles(...(rotation ?? [0, 0, 0]));
 
   return cube;
 }
@@ -25,13 +28,25 @@ const App = () => {
       antialias: true,
     });
     pixiContainer.current.appendChild(app.view);
-    app.stage.addChild(initCube([0, 0, 0]));
-    app.stage.addChild(initCube([0, 0, 1]));
+    app.stage.addChild(initCube([0, -3, 2], [0, 0, 0]));
+    app.stage.addChild(initCube([0, -3, -2], [0, 0, 0]));
+    app.stage.addChild(initCube([0, -1, 0], [-90, 0, 0]));
+    app.stage.addChild(initCube([0, -5, 0], [-90, 0, 0]));
+    app.stage.addChild(initCube([0, 1, -2], [0, 0, 0]));
 
-    let light = new PIXI3D.Light();
-    light.position.set(-3, 3, 2);
-    light.intensity = 100;
-    PIXI3D.LightingEnvironment.main.lights.push(light);
+    let dirLight = new PIXI3D.Light();
+    dirLight.type = PIXI3D.LightType.directional;
+    dirLight.intensity = 9;
+    dirLight.color = new PIXI3D.Color(0.5, 0.5, 0.5);
+    dirLight.rotationQuaternion.setEulerAngles(45, -45, 0);
+    PIXI3D.LightingEnvironment.main.lights.push(dirLight);
+
+    let dirLight2 = new PIXI3D.Light();
+    dirLight2.type = PIXI3D.LightType.directional;
+    dirLight2.intensity = 9;
+    dirLight2.color = new PIXI3D.Color(0.5,0.5,0.5);
+    dirLight2.rotationQuaternion.setEulerAngles(45, 135, 45);
+    PIXI3D.LightingEnvironment.main.lights.push(dirLight2);
 
     new PIXI3D.CameraOrbitControl(app.view);
   }, []);
