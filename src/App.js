19a55be41fc123419b43e86ed9ff69073a6b6dff
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-06-10 16:25:21 +0900

    code : 폴리곤(삼각형) 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -11,10 +11,28 @@ function initCube(scale, position, rotation, color) {
   material.metallic = 0.2;
   material.roughness = 0.7;
   cube.material = material;
-  console.log(cube.materials)
+  console.log(cube.materials);
   cube.rotationQuaternion.setEulerAngles(...(rotation ?? [0, 0, 0]));
 
   return cube;
+}
+
+function initPolygon() {
+  const geometry = Object.assign(new PIXI3D.MeshGeometry3D(), {
+    positions: {
+      buffer: new Float32Array([
+        0, -2, 0,
+        2, 0, 0,
+        0, 2, 0,
+      ]),
+    },
+  });
+
+  // 메쉬 생성 및 재질 설정
+  const material = new PIXI3D.StandardMaterial();
+  material.baseColor = new PIXI3D.Color(1, 0, 0); // 빨간색
+  const mesh = new PIXI3D.Mesh3D(geometry, material);
+  return mesh;
 }
 
 const App = () => {
@@ -27,33 +45,36 @@ const App = () => {
       resizeTo: window,
       antialias: true,
       resolution: window.devicePixelRatio || 1, // 해상도
-      autoDensity: true,           // 해상도 조정 자동화
+      autoDensity: true, // 해상도 조정 자동화
       powerPreference: "high-performance", // 성능 우선 설정
     });
     pixiContainer.current.appendChild(app.view);
-    app.stage.addChild(initCube([50, 50, 0.01], [0, -5, 0], [-90, 0, 0], [0,0,0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, -3, 2], [0, 0, 0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, -3, -2], [0, 0, 0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, -3, 0], [-90, 0, 0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, -1, 0], [-90, 0, 0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, -5, 0], [-90, 0, 0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, 1, -2], [0, 0, 0]));
-    app.stage.addChild(initCube([2, 2, 0.05], [0, 1, 0], [0, 0, 0]));
-    app.stage.addChild(initCube([2, 1, 0.05], [0, 1, -1], [-90, 0, 0]));
-    app.stage.addChild(initCube([2, 1, 0.05], [0, 3, -1], [-90, 0, 0]));
+    // app.stage.addChild(initCube([50, 50, 0.01], [0, -5, 0], [-90, 0, 0], [0.3,0.3,0.3]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, -3, 2], [0, 0, 0]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, -3, -2], [0, 0, 0]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, -3, 0], [-90, 0, 0]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, -1, 0], [-90, 0, 0]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, -5, 0], [-90, 0, 0]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, 1, -2], [0, 0, 0]));
+    // app.stage.addChild(initCube([2, 2, 0.05], [0, 1, 0], [0, 0, 0]));
+    // app.stage.addChild(initCube([2, 1, 0.05], [0, 1, -1], [-90, 0, 0]));
+    // app.stage.addChild(initCube([2, 1, 0.05], [0, 3, -1], [-90, 0, 0]));
+
+    // 삼각형 지오메트리 생성
+    app.stage.addChild(initPolygon());
 
     let dirLight = new PIXI3D.Light();
     dirLight.type = PIXI3D.LightType.directional;
-    dirLight.intensity = 9;
+    dirLight.intensity = 8;
     dirLight.color = new PIXI3D.Color(0.5, 0.5, 0.5);
-    dirLight.rotationQuaternion.setEulerAngles(45, -45, 0);
+    dirLight.rotationQuaternion.setEulerAngles(45, -40, 0);
     PIXI3D.LightingEnvironment.main.lights.push(dirLight);
 
     let dirLight2 = new PIXI3D.Light();
     dirLight2.type = PIXI3D.LightType.directional;
-    dirLight2.intensity = 9;
-    dirLight2.color = new PIXI3D.Color(0.5,0.5,0.5);
-    dirLight2.rotationQuaternion.setEulerAngles(45, 135, 45);
+    dirLight2.intensity = 4;
+    dirLight2.color = new PIXI3D.Color(0.5, 0.5, 0.5);
+    dirLight2.rotationQuaternion.setEulerAngles(0, 140, 0);
     PIXI3D.LightingEnvironment.main.lights.push(dirLight2);
 
     new PIXI3D.CameraOrbitControl(app.view);
